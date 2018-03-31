f88cbdd5a6f1664305ba0413f37fa33574dc69cb
Lahy <1245066197@qq.com>
2018-03-31 16:54:06 +0800

    Update app-routing.module.ts

--- app/app-routing.module.ts
+++ app/app-routing.module.ts
@@ -2,9 +2,13 @@ import { NgModule } from '@angular/core';
 // import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { StoryComponent } from './story/story.component';
+import { PersonComponent } from './person/person.component';
 
 const routes: Routes = [
-  { path: 'storys', component: StoryComponent }
+  { path: '', redirectTo: './story', pathMatch: 'full' },
+  { path: 'storys', component: StoryComponent },
+  { path: 'person', component: PersonComponent },
+
 ];
 
 @NgModule({
